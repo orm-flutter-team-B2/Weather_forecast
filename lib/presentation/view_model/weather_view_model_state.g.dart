0da9178cfe1e54fb5b3c1c8e55fa35f0d303e689
JEON-Sungsu <anagma@naver.com>
2024-04-18 14:51:22 +0900

    feat: viewModel state class 작성

---
+++ lib/presentation/view_model/weather_view_model_state.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weather_view_model_state.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$WeatherViewModelStateImpl _$$WeatherViewModelStateImplFromJson(
+        Map<String, dynamic> json) =>
+    _$WeatherViewModelStateImpl(
+      time:
+          (json['time'] as List<dynamic>?)?.map((e) => e as String).toList() ??
+              const [],
+      temperature: (json['temperature'] as List<dynamic>?)
+              ?.map((e) => e as num)
+              .toList() ??
+          const [],
+      weatherCode: (json['weatherCode'] as List<dynamic>?)
+              ?.map((e) => e as num)
+              .toList() ??
+          const [],
+      relativeHumidity: (json['relativeHumidity'] as List<dynamic>?)
+              ?.map((e) => e as num)
+              .toList() ??
+          const [],
+      windSpeed: (json['windSpeed'] as List<dynamic>?)
+              ?.map((e) => e as num)
+              .toList() ??
+          const [],
+    );
+
+Map<String, dynamic> _$$WeatherViewModelStateImplToJson(
+        _$WeatherViewModelStateImpl instance) =>
+    <String, dynamic>{
+      'time': instance.time,
+      'temperature': instance.temperature,
+      'weatherCode': instance.weatherCode,
+      'relativeHumidity': instance.relativeHumidity,
+      'windSpeed': instance.windSpeed,
+    };
